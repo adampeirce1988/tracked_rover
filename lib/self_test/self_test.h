46b839ebbb6205d790e69851069227903d4431bf
Adam.peirce <48071376+adampeirce1988@users.noreply.github.com>
2026-07-05 23:25:06 +0100

    test status: code compiles with 0 errors & untested on ESP.
    [verified]:
    - 100 good packet test:..........OK PROGRESS: ######################### 100%
    - 100 good packet stress test:...OK PROGRESS: ######################### 100%
    - TYPE_CHANGE....................OK PROGRESS: ######################### 100%
    - ACK_CHANGE.....................OK PROGRESS: ######################### 100%
    - ID_CHANGE......................OK PROGRESS: ######################### 100%
    - DLC_CHANGE.....................OK PROGRESS: ######################### 100%
    - DLC_OVER_MAX_CAPACITY..........OK PROGRESS: ######################### 100%
    - CRC_RAND_FLIP_BIT..............OK PROGRESS: ######################### 100%
    - CRC_CHANGE.....................OK PROGRESS: ######################### 100%
    - RAND_DATA_FLIP_BIT.............OK PROGRESS: ######################### 100%
    - RAND_DATA_CHANGE...............OK PROGRESS: ######################### 100%
    - Diagnostics WDT test:..........OK PROGRESS: ######################## 100%
    
    [FIXED]:
    - refactored self_test.h for better flow
    - updated coments to reflect recent changes
    
    [TODO]:
    - resolve all bugs in run time error log as per the reveiw!
    - clean up commented out functions and code.
    - repalce #defines for constexpr where posible.
    - run a ccode reveiw all files and refractor.
    
    [BUILD TREE]:
    tracked_rover-built-in-trasmission-self-test/
    │
    ├── platformio.ini
    ├── README.md
    ├── .gitignore
    │
    ├── src/
    │   ├── main.cpp
    │   └── TODO.txt
    │
    └── lib/
        │
        ├── config/
        │   ├── global.cpp ✅
        │   ├── global.h. ✅
        │   └── global_config.h. ✅
        │
        ├── debug/
        │   ├── debug.cpp ✅
        │   ├── debug.h ✅
        │   └── debug_config.h ✅
        │
        ├── logs/
        │   ├── logger.h ✅ (9.2/10)
        │   ├── logging_event_router.cpp ✅ (9.6/10)
        │   ├── metrics.cpp ✅ (9.8/10)
        │   ├── metrics.h ✅ (9.5/10)
        │   ├── runtime_error_log.cpp ✅ (9.3/10)
        │   ├── runtime_error_log.h ✅ (9.3/10)
        │   ├── self_test_logger.cpp ✅ (9.0/10)
        │   └── self_test_logger.h ✅ (9.2/10)
        │
        ├── protocol_layer
        │   ├── messages.h ✅ (9.4/10)
        │   ├── permissions.cpp ✅ (9.4/10)
        │   ├── permissions.h ✅ (9.7/10)
        │   ├── protocol.h ✅ (9.0/10)
        │   ├── rx_protocol.cpp ✅ (9.6/10)
        │   └── tx_protocol.cpp ✅ (9.6/10)
        │
        ├── self_test/
        │   ├── self_test_apis.h
        │   ├── self_test_fsm.cpp
        │   ├── self_test_internal.cpp
        │   ├── self_test_internal.h
        │   ├── self_test.h
        │   ├── test_diag_wdt_test.cpp
        │   ├── test_packet_error_injection.cpp
        │   └── test_packet_timing.cpp
        │
        ├── system/
        │   ├── system.cpp
        │   ├── system.h
        │   ├── vehicle_state_change.cpp
        │   └── vehicle_state_macheine.cpp
        │
        └── transport_layer/
            ├── transport.cpp
            ├── transport.h
            ├── transport_fifo.cpp
            ├── transport_uart.cpp
            └── transpot_fault_injection.cpp

--- lib/self_test/self_test.h
+++ lib/self_test/self_test.h
@@ -7,29 +7,9 @@
 #include "global_config.h"
 #include "debug.h"
 
-//=============== test status codes ================
-enum class TEST_RETURN_STATUS{
-    NO_TEST_RUNNING, 
-    TEST_RUNNING, 
-    TEST_PASSED, 
-    TEST_FAILED,
-    TEST_COMPLETED
-}; 
-
-// #define SELFTEST_NO_TEST_RUNNING                0
-// #define SELFTEST_RUNNING                        1
-// #define SELFTEST_PASSED                         2
-// #define SELFTEST_FAILED                         3 
-// #define SELFTEST_COMPLETED                      4
-// //================ ================ ================ 
-
-constexpr uint8_t ST_WDT_EXTRA_TIME =           250;   // tune to stop timout errors 
-constexpr uint8_t PROGRESS_BAR_COUNT =          25;
-//=============== gloabal variables ================
-
-// avaliavle test cases
-enum class TEST_ID : uint8_t{   // consider an enum calss for this to make calling externaly safer. 
-    RETURN_TO_IDLE = 0,           // no test in progress (default)
+//=============== avaliable test cased ================
+enum class TEST_ID : uint8_t{     
+    RETURN_TO_IDLE = 0,              // no test in progress (default: returs the state macheine back to idle)
     TRANSPORT_PACKET_NORMAL,         // Run a good packet test with moderate timing 
     TRANSPORT_PACKET_STRESS,         // run a good packet test with tight timing and random reduced time between transmisios
     TRANSPORT_TYPE_CHANGE,           // change the type wil rsult in a crc error
@@ -43,7 +23,21 @@ enum class TEST_ID : uint8_t{   // consider an enum calss for this to make calli
     TRANSPORT_DATA_BYTE_CHANGE,      // random data byte change expected crc error
     DIAGNOSTIC_WATCHDOG_TIMEOUT      // diagnostics WDT test timeout test 
 };
+
+//=============== test status codes ================
+enum class TEST_RETURN_STATUS{
+    NO_TEST_RUNNING, 
+    TEST_RUNNING, 
+    TEST_PASSED, 
+    TEST_FAILED,
+    TEST_COMPLETED
+}; 
 //================ ================ ================ 
+
+constexpr uint8_t ST_WDT_EXTRA_TIME =           250;   // tune to stop timout errors 
+constexpr uint8_t PROGRESS_BAR_COUNT =          25;
+//=============== gloabal variables ================
+
 
 //====================== API function calls  ======================
 TEST_RETURN_STATUS run_test_case();        // test state machine needs to be called every loop during a test
